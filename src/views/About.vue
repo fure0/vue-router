834801dfae8a72f78e218c5e83dc0c5b788323ef
KimTaeyoun <fure1121@surem.com>
2020-05-11 00:18:28 +0900

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div class="text-xs-center">
     This is About Page
+    <h1>click</h1>
+    <router-link :to="{name: 'home'}">
+      <h1>click</h1>      
+    </router-link>
   </div>
 </template>
