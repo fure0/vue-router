6a6bb1d382bfd63ec952302a6720c4acfc328bd8
KimTaeyoun <fure1121@surem.com>
2020-05-12 01:42:34 +0900

    add content

---
+++ src/views/Users.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Users</h1>
+        <p>this user number is {{ userId }}</p>
+        {{ $route.params.name }}
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        userId() {
+            return this.$route.params.userId
+        }
+    },
+    created() {
+        console.log("router", this.$router) // info about router.js
+        console.log("route", this.$route) // info about this page route
+    }
+}
+</script>
